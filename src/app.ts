9aa9f7ce8f0624541283dbed59263edb23df245b
Phan Soon Huat <soonhuat.phan@hotmail.com>
2022-08-28 15:40:44 +0800

    rename encypt route

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const app = express()
 const port = 3000
 
-import indexRouter from './routes/accessRoute'
+import indexRouter from './routes/encryptRoute'
 
 app.use(express.json())
 app.use(cors())
